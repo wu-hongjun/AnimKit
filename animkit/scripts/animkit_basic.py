7e39958a2b686bc1d616393b6a83a45db294abff
Hongjun Wu <hongjunwu@hotmail.com>
2020-06-16 23:50:34 -0700

    Update animkit_basic.py

--- animkit/scripts/animkit_basic.py
+++ animkit/scripts/animkit_basic.py
@@ -40,36 +40,36 @@ def win_support(filepath):
 
 # get_latest_version: Get the latest version of Maya.
 # Takes in a str MAYA_FOLDER and returns a str of VERSION in Maya, e.g. "2020"
-def get_latest_version(MAYA_FOLDER):
-    version_folders = []
-    for x in os.listdir(MAYA_FOLDER):
-        try:
-            folder = int(x)
-            version_folders += [folder]
-        except: pass
-    return str(max(version_folders))
-# Define OS type, Legacy.
-if "Darwin" in str(platform.system()):
-    # macOS is actually not supported for now because I am too lazy.
-    OS_TYPE = "Mac"
-    USER = os.environ["HOME"]
-    MAYA_FOLDER = "{0}/Library/Preferences/Autodesk/maya/".format(USER)
-    DESKTOP = os.path.expanduser("~/Desktop")
-else:
-    OS_TYPE = "Windows"
-    USER = win_support(os.getenv("USERPROFILE"))
-    MAYA_FOLDER = "{0}/Documents/maya/".format(USER)
-    DESKTOP = USER + '/Desktop'
+# def get_latest_version(MAYA_FOLDER):
+#     version_folders = []
+#     for x in os.listdir(MAYA_FOLDER):
+#         try:
+#             folder = int(x)
+#             version_folders += [folder]
+#         except: pass
+#     return str(max(version_folders))
+# # Define OS type, Legacy.
+# if "Darwin" in str(platform.system()):
+#     # macOS is actually not supported for now because I am too lazy.
+#     OS_TYPE = "Mac"
+#     USER = os.environ["HOME"]
+#     MAYA_FOLDER = "{0}/Library/Preferences/Autodesk/maya/".format(USER)
+#     DESKTOP = os.path.expanduser("~/Desktop")
+# else:
+#     OS_TYPE = "Windows"
+#     USER = win_support(os.getenv("USERPROFILE"))
+#     MAYA_FOLDER = "{0}/Documents/maya/".format(USER)
+#     DESKTOP = USER + '/Desktop'
     
-# MAYA_VERSION: e.g. 2020
-MAYA_VERSION = get_latest_version(MAYA_FOLDER)
+# # MAYA_VERSION: e.g. 2020
+# MAYA_VERSION = get_latest_version(MAYA_FOLDER)
 
-# MAYA_SCRIPT_FOLDER: Where maya stores all its scripts
-MAYA_SCRIPT_FOLDER = "{0}{1}/scripts/".format(MAYA_FOLDER,MAYA_VERSION)
+# # MAYA_SCRIPT_FOLDER: Where maya stores all its scripts
+# MAYA_SCRIPT_FOLDER = "{0}{1}/scripts/".format(MAYA_FOLDER,MAYA_VERSION)
 
 
-def load_animschool_picker():
-    mc.loadPlugin(MAYA_SCRIPT_FOLDER + 'AnimSchoolPicker.mll')
-    mel.eval('AnimSchoolPicker();')
+# def load_animschool_picker():
+#     mc.loadPlugin(MAYA_SCRIPT_FOLDER + 'AnimSchoolPicker.mll')
+#     mel.eval('AnimSchoolPicker();')
 
 ############################################################################################
