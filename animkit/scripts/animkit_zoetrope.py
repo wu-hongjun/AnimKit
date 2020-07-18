a481c427d977d0fe8582b336f226a66610c6ba0e
Hongjun Wu <hongjunwu@hotmail.com>
2020-07-18 01:16:03 -0700

    Update animkit_zoetrope.py

--- animkit/scripts/animkit_zoetrope.py
+++ animkit/scripts/animkit_zoetrope.py
@@ -5,6 +5,11 @@ import random as r
 import shutil, subprocess, sys, getpass, time, os, platform
 from mtoa.cmds.arnoldRender import arnoldRender
 
+# Version Info
+version = "2.0.0"
+update = "July 18, 2020"
+new = "Zoetrope 2.0 brings bug fixes as well as new features like automatic padding detection, frame range prompt, etc."
+
 class TimelineProperties:
     @property
     def START(self):
@@ -20,66 +25,38 @@ class TimelineProperties:
         return playbackOptions(q=1, maxTime=1)
 
 def render_frame(width, height, frame, file_format="tif", render_layer = "defaultRenderLayer"):
-    print("sceneName().parent", sceneName().parent)
-    # file_dir = os.path.join(sceneName().parent + "\\renders\\" + render_layer)).replace('\\', '/')
     file_dir = (sceneName().parent + "\\renders\\" + render_layer).replace('\\', '/')
-    print("file_dir: ", file_dir)
     prepend = os.path.basename(sceneName()).split('.')[0]
-    print("prepend: ", prepend)
     cmds.setAttr("defaultArnoldDriver.ai_translator", file_format, type="string")
     cmds.setAttr("defaultArnoldDriver.pre", file_dir + "\\" + prepend, type="string")
     cmds.setAttr("render_camShape.mask", 1)
     arnoldRender(width, height, True, True,'render_cam', ' -layer ' + render_layer)
 
-def prompt_start(renderStart, renderEnd):
+def batch_render(renderStart, renderEnd, width, height, target_format = "tif", usePadding = False, useDefaultRenderLayer = False):
+    # Prompt user to check render range.
     msg = "Will render from frame " + str(renderStart) + " to frame " + str(renderEnd) + ". Are you sure?"
-    return cmds.confirmDialog( title='Confirm Render', message=msg, button=['Yes','No'], defaultButton='Yes', cancelButton='No', dismissString='No' )
+    prompt_start = cmds.confirmDialog( title='Confirm Render', message=msg, button=['Yes','No'], defaultButton='Yes', cancelButton='No', dismissString='No' )
 
-
-def prompt_exit():
-    cmds.confirmDialog(title='Animkit Zoetrope: Task Finished.', 
-        message='Task finished. Successfully batch rendered all requested layers into target directory.', 
-        button=['I got it!'], defaultButton='I got it!', dismissString='I got it!')
-
-# Deprecated
-def fix_filename(file_name):
-    if file_name.endswith(".tif"):
-            os.rename(file_dir + "\\" + file_name, file_dir + "\\" +file_name.replace("_1.tif", ".tif"))
-    elif file_name.endswith(".png"):
-            os.rename(file_dir + "\\" + file_name, file_dir + "\\" + file_name.replace("_1.png", ".png"))
-
-def batch_render(start, end, width, height, target_format = "tif", usePadding = False, useDefaultRenderLayer = False):
-    # if usePadding:
-    #     renderStart = start - 25
-    #     renderEnd = end + 24
-    # else:
-    #     renderStart = start
-    #     renderEnd = end
-    renderStart = start
-    renderEnd = end
-
-    if prompt_start(renderStart, renderEnd) != "No":
+    if prompt_start != "No":
+        # Get list of render layers.
         render_layers = {cmds.getAttr( i + ".displayOrder") : i for i in cmds.ls(type='renderLayer')}
         render_layers.pop(0)  # Gets rid of "defaultRenderLayer"
 
+        # Set only layer to default current layer.
         if useDefaultRenderLayer: render_layers = {0: "defaultRenderLayer"}
 
+        # Batch Render.
         for index in render_layers:
-            # Batch Render
             layer = render_layers[index]
             print("[ZOETROPE]: Current Render Layer: ", layer)
             for frame in range(renderStart, renderEnd + 1):
                 cmds.currentTime(frame)
                 render_frame(width, height, frame, target_format, layer)
 
-                # Get rid of the weird _1 file name issue.
-                # for render in os.listdir(os.path.join(sceneName().parent, layer).replace('\\', '/')): fix_filename(render)
-
-        prompt_exit()
-
-
-def get_render_settings(attr):
-    return cmds.getAttr("%s.%s"%("defaultRenderGlobals", attr))
+        # Exit message.
+        cmds.confirmDialog(title='Animkit Zoetrope: Task Finished.', 
+        message='Task finished. Successfully batch rendered all requested layers into target directory.', 
+        button=['I got it!'], defaultButton='I got it!', dismissString='I got it!')
 
 def get_resolution_settings(attr):
     return cmds.getAttr("%s.%s"%("defaultResolution", attr))
